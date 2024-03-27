0987fc41dabef8715cc836e3ffa81381caccfe80
sirishtadepalli <sirishtadepalli@gmail.com>
2024-03-27 18:34:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,10 +12,22 @@ existing user to change their password.
 import hashlib  # Required for password hashing
 from datetime import datetime
 import logging
+import mysql.connector
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 
 app = Flask(__name__)
 app.secret_key = '86753093212135889'
+
+# Connect to the MySQL database
+db = mysql.connector.connect(
+    host="13.58.228.75",
+    user="cmsc495",
+    password="CMSC4952024",
+    database="cmsc495_astro"
+)
+
+#make a cursor for writing queries
+cursor = db.cursor()
 
 # Configure logging to write to security_log.txt
 security_logger = logging.getLogger("security")
@@ -43,14 +55,15 @@ failed_login_logger = logging.getLogger
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     """
-    Register users and store their information in a text file after validation.
+    Register users and store their information in a database after validation.
 
     Returns:
         rendered template for the registration page with flash messages
     """
     flash('', 'error')  # Clears existing flash messages for the session, if any
-    if request.method == 'POST':
-        username = request.form['username']
+    if request.method == 'POST': 
+        username = request.form['username'] #retrieves the values for username etc from the form that got submitted
+        name = request.form['name']
         password = request.form['password']
         confirm_password = request.form['confirm_password']
 
@@ -59,14 +72,12 @@ def register():
             flash('Password and confirm password do not match. Please try again.',
                   'error')
         else:
-            # Check if the username already exists in the file (read mode)
-            with open(USER_DATA_FILE, 'r', encoding='utf-8') as file:
-                for line in file:
-                    stored_username, _ = line.strip().split(':')
-                    if username == stored_username:
-                        flash('Username already exists. Choose another username or return'
-                              ' to Login via below link.', 'error')
-                        return render_template('register.html')
+            # Check if the username already exists in the database 
+            cursor.execute("SELECT * FROM Users WHERE username = %s" (username))
+            results = cursor.fetchall()
+            if results:
+                flash('Username already exists. Choose another username or return to Login via below link.', 'error')
+                return render_template('register.html')
 
             # Validate password complexity
             if not (len(password) >= 12 and any(c.isupper() for c in password)
@@ -74,17 +85,21 @@ def register():
                     and any(c.isdigit() for c in password)
                     and any(not c.isalnum() for c in password)):
                 flash('Password does not meet complexity requirements', 'error')
+
             # Check for spaces in username and password
             elif ' ' in username or ' ' in password:
                 flash('Username and password cannot contain spaces.', 'error')
+            
             else:
                 # Hash the password before storing it (for security)
                 hashed_password = hashlib.sha256(password.encode()).hexdigest()
 
-                # Write user data to the file (append mode)
-                with open(USER_DATA_FILE, 'a', encoding='utf-8') as file:
-                    file.write(f'{username}:{hashed_password}\n')
-                    flash('Registered! Return to Login below.', 'success')
+                # add username and hashed password to database
+                cursor.execute("INSERT INTO Users (name, username, password) VALUES (%s, %s)", name, username, hashed_password)
+
+                #commit db and display success message
+                db.commit()
+                flash('Registration successful!', 'success')
 
     return render_template('register.html')
 
@@ -104,26 +119,29 @@ def login():
         username = request.form['username']
         password = request.form['password']
 
-        # Hash the provided password for comparison
-        hashed_password = hashlib.sha256(password.encode()).hexdigest()
+        cursor.execute("SELECT password FROM Users WHERE username =  %s", (username))
+        db_password = cursor.fetchone()
+        
+        #if that password, and therefore account, exists
+        if db_password:
 
-        # Ensure username and hashed password match in user_data.txt
-        with open(USER_DATA_FILE, 'r', encoding='utf-8') as file:  # Opens file in read mode
-            for line in file:
-                stored_username, stored_hashed_password = line.strip().split(':')
-                if username == stored_username and hashed_password == stored_hashed_password:
-                    flash('Login successful', 'success')
-                    session['username'] = username
-                    print(f'User {username} logged in')  # DEBUG
-                    session['logged_in'] = True  # Sets session as logged in. Important.
-                    return redirect(url_for('home'))
+            # Hash the provided password for comparison
+            hashed_password = hashlib.sha256(db_password.encode()).hexdigest()
 
-        # Log the failed login attempt
-        failed_login_message = (f'Failed login attempt for username: {username} from '
-                                f'IP: {request.remote_addr}')
-        security_logger.info(failed_login_message)
+            if db_password != hashed_password:
+                # Log the failed login attempt
+                failed_login_message = (f'Failed login attempt for username: {username} from '
+                                    f'IP: {request.remote_addr}')
+                security_logger.info(failed_login_message)
+                flash('Invalid username or password', 'error')
 
-        flash('Invalid username or password', 'error')
+        #if there is no resulting_row, meaning that account doesn't exist
+        else:
+             # Log the failed login attempt
+                failed_login_message = ('Account not found '
+                                    f'IP: {request.remote_addr}')
+                security_logger.info(failed_login_message)
+                flash('Account not found', 'error')
 
     return render_template('login.html', reg_message=reg_message)
 
@@ -135,7 +153,7 @@ def update_password():
        candidate new one, and updates the user's password in the file if all checks pass.
 
        Returns:
-           Updates user_data.txt with user's new password, once candidate passwords pass checks.
+           Updates database with user's new password, once candidate passwords pass checks.
 
        """
     # Check if the user is logged in, if not, redirect to the login page
@@ -149,89 +167,62 @@ def update_password():
         current_password = request.form['current_password']
         new_password = request.form['new_password']
         confirm_new_password = request.form['confirm_new_password']
+        username = session.get("username")
 
-        print("Debug: username =", session.get("username"))
-        print("Debug: current_password =", current_password)
-        print("Debug: new_password =", new_password)
-        print("Debug: confirm_new_password =", confirm_new_password)
+        hashed_current_password = hashlib.sha256(current_password.encode()).hexdigest()
+        cursor.execute("SELECT password FROM Users WHERE username = %s", (username))
+        db_password = cursor.fetchone()
+        
+        # Ensure current password and database password are a match
+        if db_password != hashed_current_password:
+            # Log the failed password change attempt
+            failed_change_password_message = (f'Failed password change attempt for username: {username} from '
+                                f'IP: {request.remote_addr}')
+            security_logger.info(failed_change_password_message)
+            flash('Invalid password', 'error')
+            return redirect(url_for("update_password"))
 
-        # Ensure current password and stored password are a match
-        with open(USER_DATA_FILE, "r", encoding="utf-8") as file:
-            for line in file:
-                stored_username, stored_hashed_password = line.strip().split(":")
-                # hashed_current_password = hashlib.sha256(current_password.encode()).hexdigest()
-                if session.get("username") == stored_username:
-                    print("Debug: Found this user")
-                    if hashlib.sha256(
-                            current_password.encode()).hexdigest() != stored_hashed_password:
-                        print("Debug: Issuing flash message INCORRECT PW")
-                        flash("Current password is incorrect. Try again.",
-                              "error")
-                        return redirect(url_for("update_password"))
+        # Check for spaces in username and password
+        if " " in new_password:
+            flash("Password cannot contain spaces.",
+                    "error")
+            return redirect(url_for("update_password"))
 
-                    # Check for spaces in username and password
-                    if " " in new_password:
-                        print("Debug: Issuing flash message SPACES")
-                        flash("Password cannot contain spaces.",
-                              "error")
-                        return redirect(url_for("update_password"))
+        # Check if the new password and confirm password match
+        if new_password != confirm_new_password:
+            flash("New password and confirm password do not match. Try again.",
+                    "error")
+            return redirect(url_for("update_password"))
 
-                    # Check if the new password and confirm password match
-                    if new_password != confirm_new_password:
-                        print("Debug: Issuing flash message NEW/CONFIRM MISMATCH")
-                        flash("New password and confirm password do not match. Try again.",
-                              "error")
-                        return redirect(url_for("update_password"))
+        # Password complexity validation (same as registration)
+        if not (len(new_password) >= 12
+                and any(c.isupper() for c in new_password)
+                and any(c.islower() for c in new_password)
+                and any(c.isdigit() for c in new_password)
+                and any(not c.isalnum() for c in new_password)):
+            flash("New password does not meet complexity requirements",
+                    "error")
+            return redirect(url_for("update_password"))
 
-                    # Password complexity validation (same as registration)
-                    if not (
-                            len(new_password) >= 12
-                            and any(c.isupper() for c in new_password)
-                            and any(c.islower() for c in new_password)
-                            and any(c.isdigit() for c in new_password)
-                            and any(not c.isalnum() for c in new_password)
-                    ):
-                        print("Debug: Issuing flash message COMPLEXITY FOR THE LOSE")
-                        flash("New password does not meet complexity requirements",
-                              "error")
-                        return redirect(url_for("update_password"))
+        # Check if the new password is in CommonPasswords.txt. We're going to need this txt file.
+        with open("static/CommonPasswords.txt", "r",
+                    encoding="utf-8") as common_passwords_file:
+            print("Debug: Comparing COMMON PASSWORD LIST")
+            common_passwords = [line.strip() for line in common_passwords_file]
 
-                    # Check if the new password is in CommonPasswords.txt
-                    with open("static/CommonPasswords.txt", "r",
-                              encoding="utf-8") as common_passwords_file:
-                        print("Debug: Comparing COMMON PASSWORD LIST")
-                        common_passwords = [line.strip() for line in common_passwords_file]
+        if new_password in common_passwords:
+            flash("New password is commonly used. Choose a different password.",
+                    "error")
+            return redirect(url_for("update_password"))
 
-                    if new_password in common_passwords:
-                        print("Debug: Issuing flash message COMMON PASSWORD FOUND")
-                        flash("New password is commonly used. Choose a different password.",
-                              "error")
-                        return redirect(url_for("update_password"))
+        # Hash the new password before storing it (for security)
+        hashed_new_password = hashlib.sha256(new_password.encode()).hexdigest()
 
-                    # Hash the new password before storing it (for security)
-                    hashed_new_password = hashlib.sha256(new_password.encode()).hexdigest()
-
-                    # Update the user's password in the file
-                    updated_lines = []
-                    with open(USER_DATA_FILE, "r", encoding="utf-8") as file_read:
-                        print("Debug: Opening File to update")
-                        for line_change in file_read:
-                            stored_username, _ = line_change.strip().split(":")
-                            if session.get("username") == stored_username:
-                                print("Debug: Usernames match")
-                                updated_line = f'{session.get("username")}:{hashed_new_password}\n'
-                                updated_lines.append(updated_line)
-                                print("Debug: Line updated")
-                            else:
-                                print("Debug: Username not found, nonsense appended")
-                                updated_lines.append(line_change)
-
-                    with open(USER_DATA_FILE, "w", encoding="utf-8") as file_write:
-                        file_write.writelines(updated_lines)
-
-                    print("Debug: Issuing Flash Password updated successfully")
-                    flash("Password updated successfully", "success")
-                    return redirect(url_for("home"))
+        # Update the user's password in the database
+        cursor.execute("UPDATE Users SET password = %s WHERE username = %s", (hashed_new_password, username))
+        db.commit()
+        flash("Password updated successfully", "success")
+        return redirect(url_for("home"))
 
     return render_template("update_password.html")
 
@@ -264,6 +255,13 @@ def telescope_time():
     # Check if the user is logged in, if not, redirect to the login page
     if not session.get('logged_in'):
         return redirect(url_for('login'))
+
+    name = request.form['name']
+    date = request.form['date']
+    time = request.form['time']
+    
+    cursor.execute("")
+
 
     return render_template('telescope_time.html')
 
